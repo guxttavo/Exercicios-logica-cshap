85e4828dce259ca6060b7d8c4c0f3c336778ee78
guxttavo <gustavowbarbosas@gmail.com>
2023-03-13 11:02:37 -0300

    ex03

---
+++ Vetor/ex03/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+  -->Criar um vetor A com 8 elementos inteiros.
+  -->Construir um vetor B de mesmo tipo e tamanho e com os elementos do vetor A
+     multiplicados por 2, ou seja, B[i] = A[i] * 2
+*/
+class Program
+{
+  static void Main(string[] args)
+  {
+    int[] a = new int[3];
+    int[] b = new int[a.Length];
+
+    for (int i = 0; i < a.Length; i++)
+    {
+      Console.Write($"Insira o {i + 1} valor:");
+      a[i] = int.Parse(Console.ReadLine());
+
+      b[i] = a[i] * 2;
+    }
+
+    Console.WriteLine("Vetor a: " + string.Join(", ", a));
+    Console.WriteLine("Vetor b: " + string.Join(", ", b));
+
+  }
+}
