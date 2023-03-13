0f3cace3796e09597c471fb76baa17843ee101ac
guxttavo <gustavowbarbosas@gmail.com>
2023-03-13 15:10:57 -0300

    ex 11

---
+++ Vetor/ex11/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+-->Crie um vetor A com 10 elementos inteiros.
+-->Implementar um programa que defina e escreva a soma de todos os elementos armazenados
+   neste velor.
+*/
+class Program
+{
+  static void Main(string[] args)
+  {
+    int[] a = new int[2];
+
+    for (int i = 0; i < a.Length; i++)
+    {
+      Console.Write($"Insira o valor da posição {i} do vetor A: ");
+      a[i] = int.Parse(Console.ReadLine());
+    }
+
+    int soma = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+      soma += a[i];
+    }
+
+    Console.WriteLine("Vetor a: " + soma);
+  }
+}
