d98a1a97d28b99a364a52d9be0a7626037f36b8d
guxttavo <gustavowbarbosas@gmail.com>
2023-03-13 13:38:46 -0300

    ex08 e correcoes

--- Vetor/ex07/Program.cs
+++ Vetor/ex07/Program.cs
@@ -1,17 +1,17 @@
 ﻿/*
 -->Criar dois vetores A e B cada um com 10 elementos inteiros.
--->Construir um vetor C, onde cada elemento de C é a soma dos
-   respectivos elementos em A e B, ou seja,
-   C[i]= A[i] + B[i]
-*/
+-->Construir um vetor C, onde cada elemento de C é a subtração dos respectivos
+   elementos em A e B, ou seja,
+   C[i] = A[i] - B[i]
+ */
 using System;
 class Program
 {
   static void Main(string[] args)
   {
     int[] a = new int[3];
-    int[] b = new int[a.Length];
-    int[] c = new int[a.Length];
+    int[] b = new int[3];
+    int[] c = new int[3];
 
     for (int i = 0; i < a.Length; i++)
     {
@@ -20,12 +20,11 @@ class Program
       Console.Write($"Insira o valor da posição {i} do vetor B: ");
       b[i] = int.Parse(Console.ReadLine());
 
-      c[i] = a[i] + b[i];
+      c[i] = a[i] - b[i];
     }
 
-    Console.WriteLine("Vetor a: " + string.Join(",", a));
-    Console.WriteLine("Vetor b: " + string.Join(",", b));
-    Console.WriteLine("Vetor c: " + string.Join(",", c));
-
+    Console.WriteLine("Vetor A:" + string.Join(",", a));
+    Console.WriteLine("Vetor B:" + string.Join(",", b));
+    Console.WriteLine("Vetor C:" + string.Join(",", c));
   }
 }
