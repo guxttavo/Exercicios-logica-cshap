12aa6666d7d98d0935f36181c6d6dd49768165cb
guxttavo <gustavowbarbosas@gmail.com>
2023-03-13 10:37:27 -0300

    first commit

---
+++ Vetor/ex01/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+-->Criar um vetor A com 5 elementos inteiros.
+-->Construir um vetor B de mesmo tipo e tamanho e com os mesmos
+elementos do vetor A, ou seja, B[i] = A[i]
+ */
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    int[] a = new int[5];
+
+    for (int i = 0; i < a.Length; i++)
+    {
+      Console.Write($"Insira o {i + 1} valor:");
+      a[i] = int.Parse(Console.ReadLine());
+    }
+
+    //OBS: Vetor está sendo convertido para string
+    Console.WriteLine("Vetor a: " + string.Join(", ", a));
+
+  }
+}
---
+++ Vetor/ex01_v2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    int n;
+    int[] a;
+
+    Console.Write("Escreva a quantidade: ");
+    n = int.Parse(Console.ReadLine());
+    a = new int[n];
+
+    Console.Write("Escreva os numeros: ");
+    string[] s = Console.ReadLine().Split('o');
+
+    for (int i = 0; i < a.Length; i++)
+    {
+      a[i] = int.Parse(s[i]);
+    }
+
+    //OBS: O valor do vetor se mantém como int
+    foreach (int x in a)
+    {
+      Console.Write(x + " ");
+    }
+  }
+}
