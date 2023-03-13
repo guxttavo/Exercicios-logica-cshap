5cdd4f62a0af9802a0bb30ccf7b7334146c62c64
guxttavo <gustavowbarbosas@gmail.com>
2023-03-13 14:21:45 -0300

    ex09

---
+++ Vetor/ex09/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+-->Criar dois vetores A e B cada um com 10 elementos inteiros.
+-->Construir um vetor C, onde cada elemento de C é a divisão dos respectivos
+   elementos em A e B, ou seja,
+   C[i] = A[i] / B[i]
+ */
+class Program
+{
+  static void Main(string[] args)
+  {
+    int[] a = new int[3];
+    int[] b = new int[a.Length];
+    int[] c = new int[a.Length];
+
+    for (int i = 0; i < a.Length; i++)
+    {
+      Console.Write($"Insira o valor da posição {i} do vetor A: ");
+      a[i] = int.Parse(Console.ReadLine());
+      Console.Write($"Insira o valor da posição {i} do vetor B: ");
+      b[i] = int.Parse(Console.ReadLine());
+
+      c[i] = a[i] / b[i];
+    }
+    Console.WriteLine("Vetor A:" + string.Join(",", a));
+    Console.WriteLine("Vetor B:" + string.Join(",", b));
+    Console.WriteLine("Vetor C:" + string.Join(",", c));
+  }
+}
